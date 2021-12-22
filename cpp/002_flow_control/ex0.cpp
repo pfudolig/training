777b1df2fece3ac7d9af7c5c9b16a0f9a5a0fbf4
Paula Fudolig <paula.fudolig@gmail.com>
2021-12-22 13:19:25 -0500

    Asserted number of arguments

--- cpp/002_flow_control/ex0.cpp
+++ cpp/002_flow_control/ex0.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
 int main(int argc, char** argv) {
+  assert(argc==3);
   int start = stoi(argv[1]);
   int stop = stoi(argv[argc-1]);
 
