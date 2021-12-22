001af3d7984a26ca735b3286717b41cea6046885
Paula Fudolig <paula.fudolig@gmail.com>
2021-12-22 13:50:13 -0500

    Flow Control Exercise 2

--- cpp/002_flow_control/ex2.cpp
+++ cpp/002_flow_control/ex2.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+int abs_val(int num){
+  if(num >= 0){
+    return num;
+  } else{
+    return num * -1;
+  }
+}
 
 int main(int argc, char** argv) {
-  //write your code here
-  
+  assert(argc == 2);
+  int N = stoi(argv[1]);
+
+  for(int y = 0; y < N; y++){
+    for(int x = 0; x < N; x++){
+      cout << abs_val(x-y) << " ";
+    }
+    cout << endl;
+  }
+
   return 0;
 }
