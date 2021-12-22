9623cd97983f3b48f9ae205bd34992c334c94c34
Paula Fudolig <paula.fudolig@gmail.com>
2021-12-22 13:18:31 -0500

    Flow Control Exercise 1

--- cpp/002_flow_control/ex1.cpp
+++ cpp/002_flow_control/ex1.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <assert.h>
+using namespace std;
 
 int main(int argc, char** argv) {
-  //write your code here
+  assert(argc==3);
+  int start = stoi(argv[1]);
+  int end = stoi(argv[2]);
+
+  for(int i = start; i <= end; i++){
+    if(i%2 != 0){
+      cout << i << " ";
+      cout << " -- divisors: ";
+      
+      for(int j = 2; j < i; j++){
+        if(i%j == 0){
+          cout << j << " ";
+        }
+      }
+      cout << endl;
+    }
+
+  }
 
   return 0;
 }
