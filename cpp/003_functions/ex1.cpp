c37a409118297e4b016b78cd3ad9920fb5bc334b
Paula Fudolig <paula.fudolig@gmail.com>
2022-01-27 21:09:43 +0100

    Finishing rest of Functions Exercises

--- cpp/003_functions/ex1.cpp
+++ cpp/003_functions/ex1.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 
-//define your functions here
+int fib(int N) {
+  int first = 0;
+  int second = 1;
+  int next = 0;
+
+  for(int i=1; i<=N; i++) {
+    if(i == 1) {
+      std::cout << first << " ";
+      continue;
+    }
+    if(i == 2) {
+      std::cout << second << " ";
+      continue;
+    }
+      next = first + second;
+      first = second;
+      second = next;
+      std::cout << next << " ";
+  }
+  std::cout << std::endl;
+  std::cout << "ignore this: ";
+  return 0;
+}
 
 int main(int argc, char** argv) {
-  //write your code here
+  int N;
+
+  std::cout << "How many Fibonacci integers would you like? -> ";
+  std::cin >> N;
+
+  int nints = fib(N);
+
+  std::cout << nints << std::endl;
 
   return 0;
 }
--- cpp/003_functions/ex2.cpp
+++ cpp/003_functions/ex2.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 
-//define your functions here
+bool perf(int n) {
+  int sum = 0;
+  for(int i=1; i<n; i++) {
+    if(n % i == 0) {
+      sum = sum + i; //sum += i
+    }
+  }
+  if(sum == n) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 int main(int argc, char** argv) {
-  //write your code here
+  int start;
+  std::cout << "enter start: ";
+  std::cin >> start;
 
+  int stop;
+  std::cout << "enter stop: ";
+  std::cin >> stop;
+
+  for(int i=start; i<=stop; i++) {
+    if(perf(i)) {
+      std::cout << "Perfect number in range: " << i << std::endl;
+    }
+  }
   return 0;
 }
+
